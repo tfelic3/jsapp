0184eb7366775046b0e70c27d856e83b29276a2c
Tesalon Felicien <felicien.tesalon@gmail.com>
2020-06-24 02:06:42 -0400

    Added list of pokemon to js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,26 @@
-alert('Hello World');
-var favoriteFood = 'pizza';
-document.write(favoriteFood);
+var pokemonList = [
+	{
+		pokemonName: 'Pikachu',
+		pokemonHeight: 0.4,
+		type: 'electric',
+		weakness: 'ground',
+	},
+	{
+		pokemonName: 'Mewtwo',
+		pokemonHeight: 2,
+		type: 'psychic',
+		weakness: 'ghost',
+	},
+
+	{
+		pokemonName: 'Lugia',
+		pokemonHeight: 5.2,
+		type: 'flying',
+		weakness: 'rock',
+	},
+];
+
+console.log(pokemonList);
+document.write(pokemonList[0].pokemonName);
+document.write(pokemonList[1].pokemonName);
+document.write(pokemonList[2].pokemonName);
